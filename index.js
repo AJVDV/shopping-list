c17bd37ed3f399179e81c380d1282cb6af35ac08
Andrew VanDerVeen <69212799+AJVDV@users.noreply.github.com>
2020-08-26 13:55:44 -0700

    small changes index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$(function createItem(){
+//$(function createItem(){
     /*define a function that will create a new item with the text that was entered in the box*/
     $('#js-shopping-list-form').submit(event => {
         //make sure that the form does not try to submit data to server (we don't need that for this)
@@ -20,19 +20,19 @@ $(function createItem(){
       //empty the textbox after new entry is created
       userTextElement.val("");
     });
-});
+//});
 //currently none of this code functions...
 
 
 
 /*create a function to cross out the text in an item whose check button is clicked */
-$(function crossOut(){
+//$(function crossOut(){
     /*have jQuery listen for the click on the check button*/
     $('ul').on('click', '.shopping-item-toggle', event => {
         /*add the css class to the span with text that puts a strike through the text on that item*/
         //first acknowledge current targeted item, there has to be a way to target the list text instead of button :(
         $('.shopping-item').append("*");
-        this.closest('span').toggleClass('.shopping-item_checked');
+        $(this).closest('span').toggleClass('shopping-item_checked');
         //const targetItem = $(event.currentTarget.closest('li'));
         //state that other buttons are not targeted
         //const otherItems = $('.shopping-item').not(targetItem);
@@ -41,18 +41,18 @@ $(function crossOut(){
         //make it so that it toggles both ways
         //$('.shopping-item').toggleClass('shopping-item_checked');
     });
-});
+//});
 
 
 /*create a function that will delete an item when the delete button is clicked*/
-$(function removeItem(){
+//$(function removeItem(){
     //have it listen for the click of the delete button
     $('.shopping-list').on('click','shopping-item-delete', event => {
         //remove the item for which the delete button is clicked, need to target entire item instead of button :(
         this.closest("li").remove();
     });
-});
+//});
 
-$(createItem()); 
-$(crossOut());
-$(removeClick());
+$(createItem); 
+$(crossOut);
+$(removeClick);
